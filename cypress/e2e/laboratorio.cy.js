254386ef55352dd996b6635f251b09af58f84efb
rochyaltamirano <rochyaltamirano@gmail.com>
2026-02-23 16:12:48 -0300

    Validar precio del producto

--- cypress/e2e/laboratorio.cy.js
+++ cypress/e2e/laboratorio.cy.js
@@ -11,7 +11,12 @@ describe ('Example to-do app', ()=>{
         cy.contains('Añadir al carrito').click();
         cy.get('[data-at=cart-opener-mobile]').click();
 
-        cy.get('.py-5.overflow-auto').should('contains.text', 'Bandas Elásticas de Resistencia')
+        cy.get('.py-5.overflow-auto').should('contains.text', 'Bandas Elásticas de Resistencia');
+        cy.get('.text-black.text-center').should('have.text', 'Total: $350.00');
+
+        cy.get('[data-at=empty-cart]').click();
+        cy.get('.text-black.text-center').should('have.text', 'Total: $0.00');
+
     })
 
 })
