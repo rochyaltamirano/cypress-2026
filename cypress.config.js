33044f9f396150f55bc3a6cc2c2f4537d48b302c
rochyaltamirano <rochyaltamirano@gmail.com>
2026-04-03 00:11:11 -0300

    usos de variables de entorno

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ module.exports = {
   allowCypressEnv: false,
 
   e2e: {
+    allowCypressEnv: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/Caso1: llenar formulario.cy.js
+++ cypress/e2e/Caso1: llenar formulario.cy.js
@@ -2,13 +2,17 @@ const { log } = require("util");
 
 describe('test description', () => {
    beforeEach(() => {
-        cy.setCookie('__AUTH-TOKEN-APP','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc2NzM0YzUwLTNkMmMtNDA0NS1iODJlLWY0NGEzYmZlOGFiOSIsImlhdCI6MTc3MjI5MjI5MiwiZXhwIjoxNzc0ODg0MjkyfQ.9mHqhs1O1g8rz0xhXmMRTgVDug49_kqCOxXOrXtGy6A',
+
+        cy.visit('/');
+        cy.setCookie('__AUTH-TOKEN-APP', Cypress.env('cookie'),
             { secure: true }
         ); //en lugar del login por usuario/contraseña
         cy.visit('/form-practice');                      
     });
 
     it('Llenar formulario y enviar ', () => {
+
+        cy.log(Cypress.env('username')) //print variable de entorno
 
         cy.get('[data-at="practice-submit"]').should('be.disabled').as('botonEnviar');
 
