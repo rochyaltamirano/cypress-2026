eec62a88f0f3043cbe12b9cef3d33dccd7b4b563
rochyaltamirano <rochyaltamirano@gmail.com>
2026-04-04 18:28:38 -0300

    Agregar/quitar el mismo producto de favoritos

---
+++ cypress/e2e/cart/add-favorites.cy.js
@@ -0,0 +1,59 @@
+describe('Favoritos', () => {
+
+    beforeEach(() => {
+        cy.visit('auth/login');
+        cy.fixture('login').then((usuario) => {                   
+                cy.get('[name="email"]').type(usuario.email);
+                cy.get('[name="password"]').type(usuario.password);
+                cy.get('[data-at="submit-login"]').click();
+        })
+        cy.url().should('not.include', 'auth/login');
+   });
+
+   it('Agregar productos a favoritos', () => {     
+        cy.visit('whishlist');
+     
+        cy.get('[data-at="favorite-card"]').then(($item) =>{
+            cy.wrap($item.length).as('favorito');
+        });
+
+        cy.visit('');
+        
+        cy.get('.rounded-sm.aspect-square').first().click();
+        cy.get('[data-at="add-to-favorites"]').click();
+        cy.visit('whishlist');
+
+        cy.get('@favorito').then((favoritosLista) =>{
+        
+            cy.get('[data-at="favorite-card"]').then(($listaActualizada) => {
+                expect($listaActualizada).to.have.length(favoritosLista + 1)
+            });
+        });
+   });
+
+   it('Quitar un producto de favoritos', () => {
+        cy.visit('whishlist');
+
+        cy.get('[data-at="favorite-card"]').then(($item) =>{
+            cy.wrap($item.length).as('favorito');
+        });
+
+        cy.contains('Chaqueta Deportiva para Correr').click();
+        cy.get('[data-at="remove-from-favorites"]').click();
+        cy.visit('whishlist');
+
+        cy.get('@favorito').then((favoritosLista) =>{
+        
+            cy.get('[data-at="favorite-card"]').then(($listaActualizada) => {
+                expect($listaActualizada).to.have.length(favoritosLista - 1)
+            });
+        });
+
+     
+
+
+
+
+    
+   });
+})
