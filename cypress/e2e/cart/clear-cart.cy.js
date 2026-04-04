b7212d5ecad9c2d73999173fa869081c7aaccc3e
rochyaltamirano <rochyaltamirano@gmail.com>
2026-04-04 01:24:31 -0300

    Vaciar carrito

--- cypress/e2e/cart/clear-cart.cy.js
+++ cypress/e2e/cart/clear-cart.cy.js
@@ -4,13 +4,14 @@ describe('Flujo de limpar carrito', () => {
        cy.visit('');
     });
 
-    it('Limpiar carrito de compras', () => {
+    it('Vaciar carrito de compras', () => {
         cy.addToCart();
         cy.get('.cart-grid.items-center').should('exist');
 
         cy.get('[data-at="empty-cart"]').click({force: true});
 
         cy.get('.cart-grid.items-center').should('not.exist');
+
 
     });
 })
