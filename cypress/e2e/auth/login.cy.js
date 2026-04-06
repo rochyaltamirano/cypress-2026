9edb7984fb0167d63accd8f0cf9a1c2408d8fb5b
rochyaltamirano <rochyaltamirano@gmail.com>
2026-04-05 22:48:44 -0300

    Se agregaron casos: login fallido, verificar campos obligatorios, formato de email y redirección a crear cuenta nueva

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -22,4 +22,37 @@ describe('Flujos de login', () => {
 
         })
    });
+
+   it('Login fallido con credenciales incorrectas', () => {
+        cy.fixture('login').then((usuario) => {
+            cy.get('[name="email"]').type(usuario.email);
+            cy.get('[name="password"]').type(usuario.password + 'r'); //se agrega un caracter al password para que falle
+            cy.get('[data-at="submit-login"]').click();
+            cy.get('#swal2-html-container').should('be.visible')
+                .and('have.text', 'No pudimos iniciar sesión con estas credenciales. Intenta de nuevo.');            
+        });
+    });
+
+    it('Verificar que todos los campos sean obligatorios', () => {
+        cy.fixture('login').then((usuario) => {
+            cy.get('[name="email"]').type(usuario.email);
+            cy.get('[data-at="submit-login"]').should('be.disabled');
+            cy.get('[name="password"]').type(usuario.password + 'r'); //se agrega un caracter al password para que falle
+            cy.get('[data-at="submit-login"]').should('be.enabled');
+        });
+    });
+
+    it('Verificar el formato del email', () => {
+        const emailInvalido = 'usuario@dominio';
+        const email = 'usuario@dominio.com'
+        cy.get('[name="email"]').type(emailInvalido);
+        cy.get('.text-red-500').eq(0).should('have.text', 'Email inválido');
+        cy.get('[name="email"]').clear().type(email); //el clear limpia el campo antes de ingresar el siguiente texto
+        cy.get('.text-red-500').eq(0).should('not.be.visible');
+    });
+
+    it('Verificar redireccion del botón Crea Una', () => {
+        cy.get('.text-md.underline').click();
+        cy.url().should('includes', 'auth/signup')
+    });
 })
